ca8a898d9029b8731a29dbd2eb15d0bb4c32d64f
Angie Lara <angielara.f@msn.com>
2022-05-31 17:26:20 -0500

    Adición de un generador de contraseñas

---
+++ Aleatoriedad/generador_de_contraseñas.py
@@ -0,0 +1,25 @@
+import random
+
+def generador ():
+    mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    simbolos = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    caracteres = mayusculas + minusculas + numeros + simbolos
+    contrasena = []
+
+    for i in range(15):
+        caracter_random = random.choice(caracteres)
+        contrasena.append(caracter_random)
+    #Para convertir una lista en un string.
+    contrasena = ''.join(contrasena)
+    return contrasena
+
+def run():
+    contrasena = generador()
+    print ("Tu nueva contrasena es: "+ contrasena)
+
+
+if __name__ == "__main__":
+    run()
